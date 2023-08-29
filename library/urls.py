a099e81d0e1a2f12431db2691f8485709cdc078b
domaSaric <ds53234@fer.hr>
2023-08-29 20:13:11 +0200

    song customization

--- library/urls.py
+++ library/urls.py
@@ -5,4 +5,5 @@ from .views import SongsListView, ProfileSongsListView
 urlpatterns = [
     path("", SongsListView.as_view(), name="home"),
     path('my_library/', ProfileSongsListView.as_view(), name='mylib'),
+    path('song/<int:song_id>/', views.song, name='song')
 ]
--- library/views.py
+++ library/views.py
@@ -1,13 +1,38 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, redirect
+from django.contrib import messages
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 
 from .models import Song
+from profiles.forms import NewSongNoLinkForm
 
 def home(request):
     return render(request, template_name='library/home.html')
+
+def song(request, **kwargs):
+
+    context = {'song':Song.objects.get(id=kwargs['song_id'])}
+    
+    if request.method == 'POST':
+
+        song_form = NewSongNoLinkForm(request.POST)
+        if song_form.is_valid():
+            context['song'].lyrics = request.POST['lyrics']
+            context['song'].author = request.POST['author']
+            context['song'].title = request.POST['title']
+            context['song'].save()
+            messages.success(request, f'Song updated!')
+            return redirect('home')
+        messages.error(request, f'Song not updated!')
+        return redirect('home')
+        
+    else:
+        song_form = NewSongNoLinkForm(instance=context['song'])
+
+    context['song_form'] = song_form 
+    return render(request, template_name='library/song.html', context=context)
 
 class SongsListView(ListView):
     model = Song
